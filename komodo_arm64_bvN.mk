0ac63b018353b3eba69811f4899ba1e7eba35e26
wulan17 <galihgustip@gmail.com>
2021-07-22 19:49:33 +0000

    treble: Komodoify
    Signed-off-by: wulan17 <galihgustip@gmail.com>

---
+++ komodo_arm64_bvN.mk
@@ -0,0 +1,16 @@
+TARGET_GAPPS_ARCH := arm64
+$(call inherit-product, device/phh/treble/base-pre.mk)
+include build/make/target/product/aosp_arm64_ab.mk
+$(call inherit-product, device/phh/treble/base.mk)
+
+$(call inherit-product, vendor/komodo/config/common_full_phone.mk)
+KOMODO_GAPPS_TYPE := nogapps
+
+PRODUCT_NAME := komodo_arm64_bvN
+PRODUCT_DEVICE := phhgsi_arm64_ab
+PRODUCT_BRAND := Phh
+PRODUCT_SYSTEM_BRAND := Phh
+PRODUCT_MODEL := Phh-Treble vanilla
+
+PRODUCT_PACKAGES += 
+
